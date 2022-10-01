6b80b7c320a12fa6112fc289e10f010376672c5d
Leo Kosta <leonard.r.kosta2.mil@army.mil>
2022-09-30 22:40:23 -1000

    Added NDArray methods
    Signed-off-by: Leo Kosta <leonard.r.kosta2.mil@army.mil>

--- src/main/scala/ndarray/NDArray.scala
+++ src/main/scala/ndarray/NDArray.scala
@@ -32,7 +32,18 @@ object NDArray {
    */
   def ones[T: ClassTag](shape: List[Int]): NDArray[T] = NDArray.ofValue[T](shape, 1.asInstanceOf[T])
 
-  //TODO add apply method that just returns zeros with correct shape
+  //TODO add apply method that converts sequence to NDArray
+  //TODO how can I represent a sequence of T nested to an arbitrary level?
+  def apply[T: ClassTag](arr: Seq)
+
+  /**
+   * Returns an array whose elements are 0, 1, 2, etc. when flattened.
+   *
+   * @param shape The shape of the array. For example, an array with shape (2, 3) is a matrix with 2 rows and 3 columns.
+   * @tparam T The array element type.
+   */
+  def arange[T: ClassTag](shape: List[Int]): NDArray[T] = new NDArray[T](shape, Array.range(0, shape.product)
+    .map(_.asInstanceOf[T]))
 }
 
 /**
@@ -49,5 +60,25 @@ class NDArray[T] private (shape: List[Int], elements: Array[T]) {
    */
   def flatten(): Array[T] = elements
 
-  //TODO apply with indices in same length as shape accesses elements
+  /**
+   * Returns an element from the array.
+   *
+   * @param indices The indices to an element in the array. Must be of length [[shape.length]].
+   */
+  def apply(indices: List[Int]): T = {
+    //TODO also allow for a partial apply on arbitrary ranks
+  }
+
+  //TODO set an element in the array. Also would like to be able to set with an NDArray if only a subset of indices are provided
+
+  /**
+   * Returns an NDArray with the same elements as the input, but with the given shape.
+   *
+   * @param targetShape The shape of the output array. The product must equal [[shape.product]].
+   */
+  def reshape(targetShape: List[Int]): NDArray[T] = {
+    //TODO
+  }
+
+
 }
--- src/test/scala/ndarray/NDArraySpec.scala
+++ src/test/scala/ndarray/NDArraySpec.scala
@@ -4,8 +4,34 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class NDArraySpec extends AnyFlatSpec with Matchers {
+  "An N-dimensional array" should "have the correct number of elements" in {
+    val arr = NDArray.zeros[Int](List(2, 3, 4, 5, 6))
+    assert(arr.flatten().length == 2 * 3 * 4 * 5 * 6)
+  }
+
+  it should "default to type Float" in {
+    //TODO I'm going to have to learn the syntax a bit better to get this to pass.
+    val arr = NDArray.zeros(List(2, 3))
+    //assert(arr.flatten().forall(_.isInstanceOf[Float]))
+  }
+
+  it should "have the correct number of elements" in {
+    val arr = NDArray.zeros[Int](List(2, 3))
+    assert(arr.flatten().length == 6)
+  }
+
+  it should "return the element at the given indices" in {
+    val arr1 = NDArray.arange[Int](List(2, 3))
+    assert(arr1(List(0, 0)) == 0)
+    assert(arr1(List(0, 1)) == 1)
+    assert(arr1(List(1, 0)) == 3)
+    assert(arr1(List(1, 2)) == 5)
+    val arr2 = NDArray.arange[Int](List(2, 3, 4))
+    assert(arr2(List(0, 2, 1)) == 10)
+    assert(arr2(List(1, 1, 0)) == 16)
+  }
+
   "An NDArray.zeros array" should "contain only zeros" in {
-    //TODO we want NDArray to default to float32 if we don't specify the type
     val arr = NDArray.zeros[Int](List(2, 3))
     assert(arr.flatten().forall(_ == 0))
   }
