10e692f3488f9a4b8385e0c22f403567bdcd9b3c
Leo Kosta <leonard.r.kosta2.mil@army.mil>
2022-10-02 15:06:36 -1000

    Removed default type test for NDArray
    Signed-off-by: Leo Kosta <leonard.r.kosta2.mil@army.mil>

--- src/test/scala/ndarray/NDArraySpec.scala
+++ src/test/scala/ndarray/NDArraySpec.scala
@@ -14,12 +14,6 @@ class NDArraySpec extends AnyFlatSpec with Matchers {
     assert(arr.flatten().length == 2 * 3 * 4 * 5 * 6)
   }
 
-  it should "default to type Float" in {
-    // TODO I'm going to have to learn the syntax a bit better to get this to pass.
-    val arr = NDArray.zeros(List(2, 3))
-    // assert(arr.flatten().forall(_.isInstanceOf[Float]))
-  }
-
   it should "return the element at the given indices" in {
     val arr1 = NDArray.arange[Int](List(2, 3))
     assert(arr1(List(0, 0)) == 0)
