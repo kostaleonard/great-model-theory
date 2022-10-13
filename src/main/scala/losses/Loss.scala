ef5701bb11110be346d31c98fbd5a7447eadac59
Leo Kosta <leonard.r.kosta2.mil@army.mil>
2022-10-12 16:25:36 -1000

    Added Loss trait
    Signed-off-by: Leo Kosta <leonard.r.kosta2.mil@army.mil>

---
+++ src/main/scala/losses/Loss.scala
@@ -0,0 +1,25 @@
+package losses
+
+import ndarray.NDArray
+
+/** A loss function.
+  *
+  * The loss function is used to compute the gradient for gradient descent and
+  * other optimization algorithms.
+  *
+  * @tparam T
+  *   The array element type.
+  */
+trait Loss[T] {
+
+  /** Returns the loss on the inputs.
+    *
+    * @param y_true
+    *   The ground truth tensor of shape (batchSize, d0, d1, ..., dN).
+    * @param y_pred
+    *   The prediction tensor of shape (batchSize, d0, d1, ..., dN).
+    * @return
+    *   The loss tensor of shape (batchSize, d0, d1, ..., dN-1).
+    */
+  def compute_loss(y_true: NDArray[T], y_pred: NDArray[T]): NDArray[T]
+}
