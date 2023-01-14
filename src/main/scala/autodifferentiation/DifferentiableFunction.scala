7480f151b033b3ffb1f8811b96ecad21fac54a80
Leo Kosta <kostaleonard@gmail.com>
2023-01-14 13:49:36 -0500

    Added error handling
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- src/main/scala/autodifferentiation/DifferentiableFunction.scala
+++ src/main/scala/autodifferentiation/DifferentiableFunction.scala
@@ -1,25 +1,29 @@
 package autodifferentiation
 
 import ndarray.NDArray
+import scala.util.{Failure, Success, Try}
 
 //TODO docstring
 trait DifferentiableFunction[T] {
 
-  def compute(inputs: Map[String, NDArray[T]]): NDArray[T]
+  //TODO docstring
+  def compute(inputs: Map[String, NDArray[T]]): Try[NDArray[T]]
 
   //TODO docstring
-  //gradient of this function with respect to the arg at index
   def gradient(withRespectToInput: String): DifferentiableFunction[T]
 }
 
 case class Constant[T](value: NDArray[T]) extends DifferentiableFunction[T] {
-  override def compute(inputs: Map[String, NDArray[T]]): NDArray[T] = value
+  override def compute(inputs: Map[String, NDArray[T]]): Try[NDArray[T]] = Success(value)
 
   override def gradient(withRespectToInput: String): DifferentiableFunction[T] = Constant(NDArray.zeros(value.shape))
 }
 
 case class Input[T](name: String) extends DifferentiableFunction[T] {
-  override def compute(inputs: Map[String, NDArray[T]]): NDArray[T] = inputs(name)
+  override def compute(inputs: Map[String, NDArray[T]]): Try[NDArray[T]] = inputs.get(name) match {
+    case Some(value) => Success(value)
+    case None => Failure(new NoSuchElementException(f"Input $name is not defined"))
+  }
 
   override def gradient(withRespectToInput: String): DifferentiableFunction[T] =
     if(withRespectToInput == name) Constant(NDArray.ones(Array(1)))
@@ -27,8 +31,15 @@ case class Input[T](name: String) extends DifferentiableFunction[T] {
 }
 
 case class Add[T](a: DifferentiableFunction[T], b: DifferentiableFunction[T])(implicit num: Numeric[T]) extends DifferentiableFunction[T] {
-  override def compute(inputs: Map[String, NDArray[T]]): NDArray[T] =
-    (a.compute(inputs) + b.compute(inputs)).get
+  //TODO can I define this short circuit binary computation somewhere? This will probably be a repeated code segment if I don't. Also could clean things up.
+  override def compute(inputs: Map[String, NDArray[T]]): Try[NDArray[T]] =
+    a.compute(inputs) match {
+      case Success(aValue) => b.compute(inputs) match {
+        case Success(bValue) => aValue + bValue
+        case _ => _
+      }
+      case _ => _
+    }
 
   override def gradient(withRespectToInput: String): DifferentiableFunction[T] =
     Add(a.gradient(withRespectToInput), b.gradient(withRespectToInput))
