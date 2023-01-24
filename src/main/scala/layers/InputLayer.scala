56be926990439cb6550bc8572b9633baa02f0508
Leo Kosta <kostaleonard@gmail.com>
2023-01-24 12:24:06 -0500

    Added InputLayer docstrings
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- src/main/scala/layers/InputLayer.scala
+++ src/main/scala/layers/InputLayer.scala
@@ -3,7 +3,15 @@ import autodifferentiation.{DifferentiableFunction, Input}
 
 import scala.reflect.ClassTag
 
-//TODO docstring
+/** An input to a neural network.
+  *
+  * @param input
+  *   The placeholder input for the neural network, which is filled in with the
+  *   actual NDArray[T] elements of the (training, prediction, etc.) dataset at
+  *   run time.
+  * @tparam T
+  *   The array element type.
+  */
 case class InputLayer[T: ClassTag](input: Input[T]) extends Layer[T] {
   override def getComputationGraph: DifferentiableFunction[T] = input
 }
