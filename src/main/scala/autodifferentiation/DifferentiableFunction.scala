858bbaebccbff1ebfa3102fea2c002aa97dbc0ae
Leo Kosta <kostaleonard@gmail.com>
2023-01-25 08:27:03 -0500

    Refactored apply and slice to use arrays
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- src/main/scala/autodifferentiation/DifferentiableFunction.scala
+++ src/main/scala/autodifferentiation/DifferentiableFunction.scala
@@ -33,6 +33,7 @@ trait DifferentiableFunction[T] {
   /** Returns the set of all inputs to the function. */
   def getInputs: Set[Input[T]]
 
+  //TODO use Try
   /** Returns the output shape of the function. */
   def getOutputShape(implicit classTag: ClassTag[T]): Array[Int] =
     computeOnZeroInputs match {
@@ -184,6 +185,7 @@ case class MatMul[T](
       case failure => failure
     }
 
+  //TODO implement gradient
   override def gradient(
       withRespectToVariable: Variable[T]
   ): DifferentiableFunction[T] = ???
